b7a1efd2769e67f0ff1398f27b77c0b9b802ee76
koray öngün <koraygg28@gmail.com>
2023-10-23 20:55:10 +0300

    Proje dosyası ekle.

---
+++ Presentation/ETicaret.API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using ETicaret.Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaret.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var items = _productService.GetProducts();
+            return Ok(items);
+        }
+    }
+}
